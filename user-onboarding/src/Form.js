4c990995b8fe9f0d5e101129b439ff5a38ea4b14
Jasyn Marais <jasynmarais@gmail.com>
2019-11-14 15:31:54 +0200

    Add form inputs.

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Formik, Field, Form, useFormik } from 'formik';
+
+const UserForm = props => {
+    return (
+      <div>
+        <Formik
+          initialValues={props.initialValues}
+          render={props => {
+            return (
+              <Form>
+                <Field type='text' name='name' placeholder='Name'></Field>
+                <Field type='text' name='email' placeholder='Email'></Field>
+                <Field type='text' name='name' placeholder='Password'></Field>
+                <input type='checkbox'></input>
+                <button type='submit'>Submit</button>
+              </Form>
+            );
+          }}
+        />
+      </div>
+    );
+  };
+  
+export default UserForm;
