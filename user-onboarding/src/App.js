111c53cc824a69a055a38e07859d1ec771f90d84
Jasyn Marais <jasynmarais@gmail.com>
2020-02-21 04:44:36 +0200

    Add state to App component

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import UserForm from './Form';
 
 import initialValues from './data';
 
 function App() {
+  const [userList, setUserList] = useState(initialValues);
+  const [error, setError] = useState('');
   return (
     <div className="App">
       <UserForm initialValues={initialValues} />
