421e4a5d63aa23f69be2d8b799801d21e9559fbb
Jasyn Marais <jasynmarais@gmail.com>
2020-02-21 04:41:47 +0200

    Add form inputs

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Form from './Form';
+import UserForm from './Form';
+
+import initialValues from './data';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <UserForm initialValues={initialValues} />
     </div>
   );
 }
